96c84780c5e92ec8b459e9fc0274ed7a9ddabe7c
Romel Sikdar <102215733+RomelSikdar@users.noreply.github.com>
2025-01-16 16:25:01 +0530

    New test case added

--- test.py
+++ test.py
@@ -21,8 +21,14 @@ def test_add_multiple_numbers():
 def test_add_with_newlines(): 
     assert add("1\n2,3") == 6 
 
+# This is test string "//;\n1;2" 1 + 2 = 3
+def test_add_with_custom_delimiter(): 
+    assert add("//;\n1;2") == 3 
+    assert add("//|\n1|2|3") == 6 
+
 test_add_empty_string()
 test_add_single_number()
 test_add_two_numbers()
 test_add_multiple_numbers()
 test_add_with_newlines()
+test_add_with_custom_delimiter()
