cf3fd08104ac269dd01bb7f9f3ae123f8f57839d
Romel Sikdar <102215733+RomelSikdar@users.noreply.github.com>
2025-01-16 16:23:02 +0530

    Update test.py
    new test case added

--- test.py
+++ test.py
@@ -9,5 +9,10 @@ def test_add_empty_string():
 def test_add_single_number(): 
     assert add("1") == 1 
 
+# This is test string "1,2" 1 + 2 = 3
+def test_add_two_numbers(): 
+    assert add("1,2") == 3 
+
 test_add_empty_string()
 test_add_single_number()
+test_add_two_numbers()
