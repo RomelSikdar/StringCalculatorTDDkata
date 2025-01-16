a98b0d360e1122e95209bdb271b3a6c437dd3709
Romel Sikdar <102215733+RomelSikdar@users.noreply.github.com>
2025-01-16 16:23:53 +0530

    new test case added

--- test.py
+++ test.py
@@ -13,6 +13,11 @@ def test_add_single_number():
 def test_add_two_numbers(): 
     assert add("1,2") == 3 
 
+# This is test string "1,2,3,4" 1 + 2 + 3 + 4 = 10
+def test_add_multiple_numbers(): 
+    assert add("1,2,3,4") == 10 
+
 test_add_empty_string()
 test_add_single_number()
 test_add_two_numbers()
+test_add_multiple_numbers()
