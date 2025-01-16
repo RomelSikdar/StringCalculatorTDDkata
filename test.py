f705a996e2eb3120cf9e732e4bd79743a0428699
Romel Sikdar <102215733+RomelSikdar@users.noreply.github.com>
2025-01-16 16:22:27 +0530

    Update test.py
    new test case updated

--- test.py
+++ test.py
@@ -5,4 +5,9 @@ import pytest
 def test_add_empty_string(): 
     assert add("") == 0
 
+# This is test string "1"
+def test_add_single_number(): 
+    assert add("1") == 1 
+
 test_add_empty_string()
+test_add_single_number()
